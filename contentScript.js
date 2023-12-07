0635030b0a225c5d9f485b4ed01fd4e6e3efeb8b
Matt Hart <mhart1992@gmail.com>
2023-12-07 14:02:41 -0500

    added attribution to html footer

--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,18 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
     let rawFetch;
+
     await fetch("https://api.themotivate365.com/stoic-quote")
         .then((res) => res.json())
         .then((data) => {
             rawFetch = data;
         });
-
     const myQuote = await rawFetch;
-
     const body = document.querySelector('body');
-
     const newDiv = document.createElement('div');
     newDiv.innerText = myQuote.quote;
-
     const author = document.createElement('p');
     author.innerText = myQuote.author;
-
-
     body.appendChild(newDiv);
     body.appendChild(author);
-
 });
